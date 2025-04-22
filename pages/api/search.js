4035529d964ab4ee951e7811340f9d7310c0ae1d
GulliGeir <79102205+GulliGeir@users.noreply.github.com>
2025-04-22 21:37:01 +0000

    Update search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   try {
     // Flights (example using Travelpayouts API)
     if (includeFlights) {
-      const flightUrl = `https://api.travelpayouts.com/v1/prices/cheap?origin=${origin}&destination=KEF&depart_date=${departureDate}&return_date=${returnDate}&token=YOUR_TRAVELPAYOUTS_TOKEN`;
+      const flightUrl = `https://api.travelpayouts.com/v1/prices/cheap?origin=${origin}&destination=KEF&depart_date=${departureDate}&return_date=${returnDate}&token=2352229ce17e3f55dda5315050907762`;
       const flightRes = await fetch(flightUrl);
       const flightData = await flightRes.json();
       results.flights = flightData.data.KEF || [];
